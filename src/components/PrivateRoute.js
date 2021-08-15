5e5b18f73809952f994a22a78d80247a40107b1b
Matthew Miller <mmlak_0109@yahoo.com>
2021-08-16 06:18:21 +0900

    move comps to own folder

---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,13 @@
+import { Redirect, Route } from "react-router-dom";
+
+export default function PrivateRoute(props) {
+  const { path, exact, component: Component, signedin } = props;
+
+  return (
+    <Route exact={exact} path={path} render={routeProps => (
+      signedin
+        ? <Component {...routeProps} />
+        : <Redirect to='/sign-in' />
+    )} />
+  )
+};
