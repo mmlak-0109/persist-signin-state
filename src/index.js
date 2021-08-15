2488c7c859ca9b97bf246207cf334a52ed78a221
Matthew Miller <mmlak_0109@yahoo.com>
2021-08-16 06:14:19 +0900

    feat: add PersistGate for state persist

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import './index.css';
 import store from './app/store';
 import { Provider } from 'react-redux';
 import App from './App';
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
+
+let persistor = persistStore(store);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
