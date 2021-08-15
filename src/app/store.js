925344701bf95c70ad7121b3a305ba2b00b53ab9
Matthew Miller <mmlak_0109@yahoo.com>
2021-08-16 06:13:40 +0900

    feat: add redux-persist

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,35 @@
+import { combineReducers } from 'redux'
 import { configureStore } from '@reduxjs/toolkit'
 import authedUserReducer from '../features/authedUser/authedUserSlice'
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+}
+
+const reducer = combineReducers({
+  authedUser: authedUserReducer
+})
+
+const persistedReducer = persistReducer(persistConfig, reducer)
 
 export default configureStore({
-  reducer: {
-    authedUser: authedUserReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 })
