e05fb701968e5e308f6bf4b249952d6c93b178f8
Matthew Miller <mmlak_0109@yahoo.com>
2021-08-16 03:43:52 +0900

    feat: select state and add routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import SignIn from './SignIn';
+import Home from './Home';
+import PrivateRoute from './PrivateRoute'
+import { useSelector } from 'react-redux'
 
-function App() {
+const App = () => {
+
+  const signedIn = useSelector((state) => state.authedUser.value !== "")
+  const user = useSelector((state) => state.authedUser.value)
+
+  console.log(signedIn)
+  console.log(user)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Router>
+      <>
+        <div className='container'>
+          <Switch>
+            <div>
+              <PrivateRoute path='/' exact component={Home} signedin={signedIn} />
+              <Route path='/sign-in' exact component={SignIn} />
+            </div>
+          </Switch>
+        </div>
+      </>
+    </Router>
+  )
+};
 
 export default App;
